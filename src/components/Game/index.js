c55fc892c12e3b12ce20a5006378770bf7b86024
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-01-28 18:49:55 +0300

    a

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./styles.css";
 
 function Game() {
-  return <div id="game">ddd</div>;
+  return (
+    <div id="game">
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
